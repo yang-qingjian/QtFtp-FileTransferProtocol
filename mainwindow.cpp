fba7c5538a57abeb0ad7df9eec66d4381828e01f
杨青健 <33346151+yang-qingjian@users.noreply.github.com>
2020-03-04 00:30:07 +0800

    Add files via upload
    # 修正内容：对于mainwindow测试代码：
    #       初始化是返回按钮，使初始化使为false
    #       下载文件时，如果选择的是目录则进行报错
    #       添加了详细的注释

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QTextCodec>
 #include <QFile>
 #include <QTreeWidgetItem>
+#include <QDebug>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -12,7 +13,12 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    //初次打开软件默认返回上一级按钮不可用
+    ui->cdToParentButton->setEnabled(false);
+
     ui->progressBar->setValue(0);
+
+    //如果我们鼠标双击了这个目录，则会发出这个信号
     connect(ui->fileList, &QTreeWidget::itemActivated,
             this, &MainWindow::processItem);
 }
@@ -22,6 +28,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//命令开始
 void MainWindow::ftpCommandStarted(int)
 {
     int id = ftp->currentCommand();
@@ -41,6 +48,8 @@ void MainWindow::ftpCommandStarted(int)
     }
 }
 
+//跟进执行不同的命令，来判断执行结果，如果执行出错会返回一个bool值
+//跟进bool判断需要显示的结果，errorString如果错误，则记录错误信息
 void MainWindow::ftpCommandFinished(int, bool error)
 {
     if(ftp->currentCommand() == QFtp::ConnectToHost) {
@@ -78,40 +87,69 @@ void MainWindow::ftpCommandFinished(int, bool error)
 // 连接按钮
 void MainWindow::on_connectButton_clicked()
 {
+    //清理工作
     ui->fileList->clear();
     currentPath.clear();
     isDirectory.clear();
     ui->progressBar->setValue(0);
+    //创建FTP对象
     ftp = new QFtp(this);
+    //连接信号和槽：当指令 开始 的时候，刷新指令的显示状态
     connect(ftp, &QFtp::commandStarted, this,
             &MainWindow::ftpCommandStarted);
+    //连接信号和槽：当指令 完成 的时候，刷新指令的显示状态
     connect(ftp, &QFtp::commandFinished,
             this, &MainWindow::ftpCommandFinished);
+    //连接信号和槽：读取到列表信息的时候，通知保存列表信息
     connect(ftp, &QFtp::listInfo, this, &MainWindow::addToList);
+    //连接信号和槽：当有下载进度的时候，更新进度条显示
     connect(ftp, &QFtp::dataTransferProgress,
             this, &MainWindow::updateDataTransferProgress);
+
+    //获取用户输入的FTP地址，用户名，密码
     QString ftpServer = ui->ftpServerLineEdit->text();
     QString userName = ui->userNameLineEdit->text();
     QString passWord = ui->passWordLineEdit->text();
+    //连接到ftp服务器，指定地址，端口（ftp端口默认是21所以可以写死）
     ftp->connectToHost(ftpServer, 21);
+    //使用密码登录ftp服务器
     ftp->login(userName, passWord);
 }
 
+//获取列表信息
 void MainWindow::addToList(const QUrlInfo &urlInfo)
 {
     // 注意：因为服务器上文件使用UTF-8编码，所以要进行编码转换，这样显示中文才不会乱码
+    //编码转换
     QString name = QString::fromUtf8(urlInfo.name().toLatin1());
     QString owner = QString::fromUtf8(urlInfo.owner().toLatin1());
     QString group = QString::fromUtf8(urlInfo.group().toLatin1());
+
+    //定义一个树形部件，创建一个树形条目，下面是为部件设置“字段”（类似一个表格）
     QTreeWidgetItem *item = new QTreeWidgetItem;
+    //第1 字段是：文件名称
     item->setText(0, name);
+    //第2 字段是：文件大小
     item->setText(1, QString::number(urlInfo.size()));
+    //第3 字段是：拥有者
     item->setText(2, owner);
+    //第4 字段是：用户组
     item->setText(3, group);
+    //第5 字段是：最后修改日期
     item->setText(4, urlInfo.lastModified().toString("yyyy-MM-dd"));
+
+    //设置文件图标：判断当前info，如果是目录，则文件名称前面显示目录的图片，
+    //如果是文件则文件名前显示文件的图片
     QPixmap pixmap(urlInfo.isDir() ? "../myFTP/dir.png" : "../myFTP/file.png");
+    //把上面的图标设置为图标
     item->setIcon(0, pixmap);
+
+    //把是不是目录的信息，保存到哈希表中isDirectory中
+    //（QHash<QString, bool> isDirectory;）是一个键值对
+    //为了是后面用户点击了这个文件，需要根据是目录还是文件做相应的操作
     isDirectory[name] = urlInfo.isDir();
+
+    //把上面当前这个条目的内容，添加到属性目录中（当前是一条一条获取的，也是一条一条添加的）
     ui->fileList->addTopLevelItem(item);
     if (!ui->fileList->currentItem()) {
         ui->fileList->setCurrentItem(ui->fileList->topLevelItem(0));
@@ -119,18 +157,26 @@ void MainWindow::addToList(const QUrlInfo &urlInfo)
     }
 }
 
+//双击后对当条目进行处理，如果是目录则进入内部，如果是文件，则不处理
 void MainWindow::processItem(QTreeWidgetItem *item, int)
 {
+
+    qDebug()<<"***item激活提示*****";   //验证是单击进入，还是双击进入
     // 如果这个文件是个目录，则打开
     if (isDirectory.value(item->text(0))) {
         // 注意：因为目录名称可能是中文，在使用ftp命令cd()前需要先进行编码转换
         QString name = QLatin1String(item->text(0).toUtf8());
         ui->fileList->clear();
         isDirectory.clear();
+        //记录生成当前的最新目录
         currentPath += "/";
         currentPath += name;
+        //进入新的文件夹
         ftp->cd(name);
+        qDebug()<<"********当前进入的路径："<<currentPath;
+        //获取内容列表
         ftp->list();
+        //设置返回上级按钮为可用
         ui->cdToParentButton->setEnabled(true);
     }
 }
@@ -138,9 +184,18 @@ void MainWindow::processItem(QTreeWidgetItem *item, int)
 // 返回上级目录按钮
 void MainWindow::on_cdToParentButton_clicked()
 {
+    if(!ui->cdToParentButton->isEnabled())
+    {
+        qDebug()<<"**********返回上一级文件夹不可用***********";
+        return;
+    }
+    //清理内容
     ui->fileList->clear();
     isDirectory.clear();
+    //按斜线分割，找到上一级目录
     currentPath = currentPath.left(currentPath.lastIndexOf('/'));
+    qDebug()<<"********当前返回的路径："<<currentPath;
+    //如果上一级目录为空，则进入根目录，否则切换到最新目录
     if (currentPath.isEmpty()) {
         ui->cdToParentButton->setEnabled(false);
         ftp->cd("/");
@@ -154,17 +209,30 @@ void MainWindow::on_cdToParentButton_clicked()
 void MainWindow::on_downloadButton_clicked()
 {
     // 注意：因为文件名称可能是中文，所以在使用get()函数前需要进行编码转换
+    //获取选择当前选中的文件的名称，并针对中文进行转码
     QString fileName = ui->fileList->currentItem()->text(0);
     QString name = QLatin1String(fileName.toUtf8());
+
+    if (isDirectory.value(name)) {
+        qDebug()<<"********下载的是目录："<<name;
+        ui->label->setText("下载出现错误：不支持下载目录!");
+        return;
+    }
+    qDebug()<<"********下载的是文件："<<name;
+
+    //创建一个文件，关联文件名
     file = new QFile(fileName);
     if (!file->open(QIODevice::WriteOnly)) {
         delete file;
         return;
     }
+    //开始下载，设置下载按钮不可用
     ui->downloadButton->setEnabled(false);
+    //下载文件
     ftp->get(name, file);
 }
 
+//更新进度条
 void MainWindow::updateDataTransferProgress(qint64 readBytes,qint64 totalBytes)
 {
     ui->progressBar->setMaximum(totalBytes);
